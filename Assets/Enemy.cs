25c831d6e928ecee63a3d5fef8238b3e3f5ae9f3
Shunsuke Kimura <me@kimurashunsuke.com>
2022-02-05 20:09:28 +0900

    コミットし忘れ

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,36 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player1 : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
     private float speed;
-    private AudioClip audioClip;
+    
     void Start()
     {
-        audioClip = gameObject.GetComponent<AudioSource> ().clip;
-
         myRigidbody = this.gameObject.GetComponent<Rigidbody2D>();        
-        speed = 10f;
+        speed = -0.04f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            speed += 6f;
-            Vector2 force = new Vector2(speed, 0f);
-            myRigidbody.AddForce(force);
-        }
+        speed -= 0.001f;
+        Vector2 force = new Vector2(speed, 0f);
+        myRigidbody.AddForce(force);
     }
-
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.name == "GameEndGround") {
             SceneManager.LoadScene("Title");
-        }
-        if (other.gameObject.name == "Enemy") {
-            GetComponent<AudioSource>().PlayOneShot(audioClip);
         }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player2 : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
     private float speed;
-    
+    private AudioClip audioClip;
     void Start()
     {
+        audioClip = gameObject.GetComponent<AudioSource> ().clip;
+
         myRigidbody = this.gameObject.GetComponent<Rigidbody2D>();        
-        speed = -0.04f;
+        speed = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed -= 0.001f;
-        Vector2 force = new Vector2(speed, 0f);
-        myRigidbody.AddForce(force);
+        if (Input.GetKeyDown("space"))
+        {
+            speed += 8;
+            Vector2 force = new Vector2(speed, 0f);
+            myRigidbody.AddForce(force);
+        }
     }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.name == "GameEndGround") {
             SceneManager.LoadScene("Title");
+        }
+        if (other.gameObject.name == "Enemy") {
+            GetComponent<AudioSource>().PlayOneShot(audioClip);
         }
     }
 }
